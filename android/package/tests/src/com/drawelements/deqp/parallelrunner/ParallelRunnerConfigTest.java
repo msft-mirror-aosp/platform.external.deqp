712a962c6f78d54640fdfaf8eed0863652af6f8f
Surya Velaga <saisuryavelaga@google.com>
2026-07-02 09:06:45 +0000

    dEQP: Integrate parallel test runner into Instrumentation
    Integrates the dEQP parallel test runner into Android Instrumentation
    framework. Adds support for argument passing(deqpParallel, deqpMaxWorkers,
    deqpTestBatchesDir), activity routing in RemoteAPI, and lifecycle latch
    synchronization.
    
    Bug: 527344038
    Test: atest DeqpAppTests, cts -m CtsDeqpTestCases
    Ignore-Khronos-First: Experimental change
    Flag: EXEMPT TEST_ONLY
    Change-Id: I5f109eea65dab7cdbef45af7d954c9dca10e5769

---
+++ android/package/tests/src/com/drawelements/deqp/parallelrunner/ParallelRunnerConfigTest.java
@@ -0,0 +1,44 @@
+/*-------------------------------------------------------------------------
+ * drawElements Quality Program Tester Core
+ * ----------------------------------------
+ *
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.drawelements.deqp.parallelrunner;
+
+import static org.junit.Assert.assertEquals;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Unit tests for {@link ParallelRunnerConfig}.
+ */
+@RunWith(AndroidJUnit4.class)
+public class ParallelRunnerConfigTest {
+
+    @Test
+    public void testDefaultMaxWorkersConstant() {
+        assertEquals(4, ParallelRunnerConfig.DEFAULT_MAX_WORKERS);
+    }
+
+    @Test
+    public void testMaxAllowedWorkersConstant() {
+        assertEquals(12, ParallelRunnerConfig.MAX_ALLOWED_WORKERS);
+    }
+}
